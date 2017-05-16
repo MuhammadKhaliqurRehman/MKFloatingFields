7a599976c44631619894f817c9fbbbe3755688fb
MuhammadKhaliqurRehman <khaliq.bese18@gmail.com>
2017-05-16 22:48:05 +0500

    Initial Commit

---
+++ MKFloatingFields/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MKFloatingFields
+//
+//  Created by Muhammad Khaliq ur Rehman on 16/05/2017.
+//  Copyright © 2017 Muhammad Khaliq ur Rehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
